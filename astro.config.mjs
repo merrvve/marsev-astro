c7a73525cf46fc0a56b3490dd970a9268e1869e0
mervs <mervsen@gmail.com>
2025-08-12 12:19:27 +0300

    fixed youtube import

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,6 @@ export default defineConfig({
         "@/shortcodes/Accordion",
         "@/shortcodes/Notice",
         "@/shortcodes/Video",
-        "@/shortcodes/Youtube",
         "@/shortcodes/Tabs",
         "@/shortcodes/Tab",
       ],
