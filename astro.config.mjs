617a08fa745c7904f3c604649cb2183cf6f9da6f
mervs <mervsen@gmail.com>
2026-01-15 14:59:03 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: "https://marsev.org",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: {
@@ -25,6 +25,7 @@ export default defineConfig({
     }
   },
   integrations: [
+    
     react(),
     sitemap(),
     tailwind({
